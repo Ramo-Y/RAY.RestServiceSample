bd731c293269722368521921ced8fe793aa914e5
Ramo-Y <r_yilmaz@outlook.com>
2019-09-12 17:36:37 +0200

    Add startup project

---
+++ source/CmdApi/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CmdApi
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
+    }
+}
