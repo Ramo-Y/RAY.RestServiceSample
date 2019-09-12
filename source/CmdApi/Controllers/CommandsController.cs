f59ae1570ffdceb1b119f95a8409ba2ea2f737ef
Ramo-Y <r_yilmaz@outlook.com>
2019-09-12 17:50:50 +0200

    Add first controller and disable ssl

---
+++ source/CmdApi/Controllers/CommandsController.cs
@@ -0,0 +1,20 @@
+﻿namespace CmdApi.Controllers
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetString()
+        {
+            return new[]
+                       {
+                           "this", "is", "hard", "coded"
+                       };
+        }
+    }
+}
--- source/CmdApi/Startup.cs
+++ source/CmdApi/Startup.cs
@@ -17,6 +17,7 @@
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMvc();
         }
     }
 }
